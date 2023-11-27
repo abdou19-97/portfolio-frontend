20ccee899bcc5ba9fc6880d6a9298ae085480f96
diyam <diyamed98@gmail.com>
2023-11-27 16:35:00 -0500

    Home commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,13 @@ import React from "react";
 import MasterPage from "../components/MasterPage";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Mywork from "../project/Mywork";
+// import Mywork from "../project/Mywork";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <MasterPage />
-      <Mywork />
+      {/* <Mywork /> */}
       <Footer />
     </div>
   );
